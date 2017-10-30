b9a334eb8046aa552ea2c57d0816b31267a69ff3
Jacob Mason <jacob@jacobmason.net>
2017-10-30 13:09:15 -0500

    Setup jwt token, default role, and schema as env vars
    Signed-off-by: Jacob Mason <jacob@jacobmason.net>

--- connect-to-docker.sh
+++ connect-to-docker.sh
@@ -6,5 +6,9 @@ fi
 
 postgraphql --host 0.0.0.0 --cors $POSTGRAPHQL_GRAPHIQL_DISABLED \
  --connection postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@$POSTGRES_HOSTNAME:$POSTGRES_PORT \
+ --schema "$POSTGRAPHQL_SCHEMAS_MONITORED" \
+ --jwt-secret "$JWT_SECRET" \
+ --default-role $POSTGRES_USER \
+ --token "$POSTGRAPHQL_JWT_TOKEN_TYPE" \
  --watch \
  1>/dev/null
